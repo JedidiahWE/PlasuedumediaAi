030408960d9ab74fb7a62d83c7eaca3ac6f5b54b
JedidiahWE <jedidiahweng@gmail.com>
2026-07-05 18:01:59 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import TrustedBy from "../../components/TrustedBy/TrustedBy";
 import Features from "../../components/Features/features";
-import HowItWorks from "../../components/howitworks/howItworks";
+import HowItWorks from "../../components/howitworks/howitworks";
 import Services from "../../components/Services/Services";
 import Stats from "../../components/Stats/Stats";
 import Testimonials from "../../components/Testimonials/Testimonials";
