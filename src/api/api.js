0520ca3c115d5624820a7a06077de9ac0da0439e
JedidiahWE <jedidiahweng@gmail.com>
2026-07-08 12:03:46 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://plasuedumediaai.onrender.com",
-});
+  baseURL: "https://plasuedumediaai.onrender.com/api",
 
 api.interceptors.request.use((config) => {
 
