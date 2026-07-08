13675f8cb9a75802f77574efd7197c78562a2c77
JedidiahWE <jedidiahweng@gmail.com>
2026-07-08 12:23:41 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,17 +2,22 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://plasuedumediaai.onrender.com/api",
-
-api.interceptors.request.use((config) => {
-
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  headers: {
+    "Content-Type": "application/json"
   }
-
-  return config;
-
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
